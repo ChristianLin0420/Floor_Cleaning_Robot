6b768e5d993444524e5ceed0b2ff114c3dcd086c
BJLin <crlc112358@gmail.com>
2019-11-03 16:06:44 +0800

    Initial Commit

---
+++ Floor_Cleaning_Robot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Floor_Cleaning_Robot
+//
+//  Created by Christian on 2019/11/3.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
